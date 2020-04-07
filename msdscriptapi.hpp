c24e3fa2125576c6a515eb6b9186fd5b6d15f672
xiangjieli <xiangjieli1997@gmail.com>
2020-04-06 23:18:20 -0600

    latest version

---
+++ msdscriptapi.hpp
@@ -0,0 +1,12 @@
+#ifndef parse_hpp
+#define parse_hpp
+
+#include <iostream>
+
+std::string interp(std::string s);
+std::string stepInterp(std::string s);
+std::string optimize(std::string s);
+bool equals(std::string s1, std::string s2);
+
+
+#endif /* parse_hpp */
